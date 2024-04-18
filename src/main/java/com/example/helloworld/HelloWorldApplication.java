16a9403b1d68e0a952307249e5a73d6f47f0a08a
tcheukh5928 <hervetcheukado@webheadtech.com>
2024-04-17 23:20:15 -0500

    reverted to Alfred last commit

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -11,7 +11,7 @@ public class HelloWorldApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello, World Update! Good evening";
+        return "Hello, World Update!";
     }
 
     public static void main(String[] args) {
